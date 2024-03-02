b0b14b925032dd9950ee8c485a0a3acca9a0bf9f
Rahulyadav872 <161304575+Rahulyadav872@users.noreply.github.com>
2024-03-02 11:04:08 +0530

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,16 @@
+r#taks 3
+#print("Hello World")
+#Nested if/elif
+# if employee have money=40000000=A grade
+#if less then employee=miser
+amount=int(input("Enter your amount: "))
+if amount>=40000000:
+  print("good employee")
+elif amount<40000000 and amount >=30000000:
+  print("bad employee")
+elif amount<30000000 and amount >=20000000:
+  print ("very bad employee")
+
+else:
+  print(" no information ")
+  
